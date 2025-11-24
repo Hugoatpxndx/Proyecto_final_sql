d981d49bd1a36e86c446f9183c61392bf6549ff9
Hugo Ruiz <davidhugorm@gmail.com>
2025-11-24 12:03:22 -0600

    Crear APP

---
+++ src/APP.java
@@ -0,0 +1,12 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ResultSet;
+import java.sql.Statement;
+
+public class APP {
+    static final
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
